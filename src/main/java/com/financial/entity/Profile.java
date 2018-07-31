4c09578dab305f694b9e28d60e4c134ca222a0bd
SkyHappiness <929506862@qq.com>
2018-07-31 15:17:50 +0800

    增加昨日收益属性

--- src/main/java/com/financial/entity/Profile.java
+++ src/main/java/com/financial/entity/Profile.java
@@ -5,52 +5,71 @@ package com.financial.entity;
  * time:   2018-7-31
  */
 public class Profile {
-	
+
 	private Integer profileId;
 	private Integer profileProId;
 	private String profileProName;
+	private Double profileYestoday;
 	private Double profileMoney;
 	private Integer flag;
-	
+
 	public Profile() {
 		this.flag = 1;
 	}
-	
+
 	public Integer getProfileId() {
 		return profileId;
 	}
+
 	public void setProfileId(Integer profileId) {
 		this.profileId = profileId;
 	}
+
 	public Integer getProfileProId() {
 		return profileProId;
 	}
+
 	public void setProfileProId(Integer profileProId) {
 		this.profileProId = profileProId;
 	}
+
 	public String getProfileProName() {
 		return profileProName;
 	}
+
 	public void setProfileProName(String profileProName) {
 		this.profileProName = profileProName;
 	}
+
 	public Double getProfileMoney() {
 		return profileMoney;
 	}
+
 	public void setProfileMoney(Double profileMoney) {
 		this.profileMoney = profileMoney;
 	}
+
 	public Integer getFlag() {
 		return flag;
 	}
+
 	public void setFlag(Integer flag) {
 		this.flag = flag;
 	}
+
+	public Double getProfileYestoday() {
+		return profileYestoday;
+	}
+
+	public void setProfileYestoday(Double profileYestoday) {
+		this.profileYestoday = profileYestoday;
+	}
+
 	@Override
 	public String toString() {
 		return "Profile [profileId=" + profileId + ", profileProId=" + profileProId + ", profileProName="
-				+ profileProName + ", profileMoney=" + profileMoney + ", flag=" + flag + "]";
+				+ profileProName + ", profileYestoday=" + profileYestoday + ", profileMoney=" + profileMoney + ", flag="
+				+ flag + "]";
 	}
-	
-	
+
 }
